bd3e4238a792607b73f97e31f0d82f846b4575f4
Vikranth Srivatsa <rama22446@gmail.com>
2018-01-20 21:54:43 -0800

    Change HTTPS to HTTP

--- app/mobile/views.py
+++ app/mobile/views.py
@@ -75,7 +75,7 @@ def mobile_google_login():
     email = data.get('email')
     full_name = data.get('full_name')
     import requests
-    user_id = requests.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token={}'.format(google_id_token)).json().get('sub')
+    user_id = requests.get('http://www.googleapis.com/oauth2/v3/tokeninfo?id_token={}'.format(google_id_token)).json().get('sub')
     if user_id is None:
         return jsonify({"error": "Incorrect Google Login Token. Please Try Again"}), 401
 
