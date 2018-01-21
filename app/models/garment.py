06f63385ac8ecf0f3304456451afc4c745819912
Vikranth Srivatsa <rama22446@gmail.com>
2018-01-21 01:53:18 -0800

    Setup user_id

--- app/models/garment.py
+++ app/models/garment.py
@@ -2,10 +2,12 @@
 
 from app import db
 
+
 class Garment(db.model):
     __table_name__ = 'Garment'
     id = db.Column(db.Integer, primary_key=True, unique=True)
     img_url = db.Column(db.String(350), unique=True)
+    user_id = db.Column(db.String(350))
     tags = db.relationship('Tag', backref='garment', lazy='dynamic')
 
     def __repr__(self):
