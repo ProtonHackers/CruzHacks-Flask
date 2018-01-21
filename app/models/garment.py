dde3434f50acaf13640aec31d69f2680494e3556
Vikranth Srivatsa <rama22446@gmail.com>
2018-01-21 09:37:05 +0000

    Server Fixes

--- app/models/garment.py
+++ app/models/garment.py
@@ -3,10 +3,11 @@
 from app import db
 
 class Garment(db.Model):
-    __table_name__ = 'Garment'
+    __table_name__ = 'Garment' 
+    extend_existing = True
     id = db.Column(db.Integer, primary_key=True, unique=True)
     user_id = db.Column(db.Integer)
     img_url = db.Column(db.String(350), unique=True)
 
     def __repr__(self):
-        return '<Garment {}>'.format(self.garment_id)
+        return '<Garment {}>'.format(self.id)
