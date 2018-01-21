00810830127a32ccf1115a3aff8883399597518a
Vikranth Srivatsa <rama22446@gmail.com>
2018-01-21 01:45:44 -0800

    update views with jsonify

--- app/models/garment.py
+++ app/models/garment.py
@@ -2,12 +2,11 @@
 
 from app import db
 
-class Garment(db.Model):
+class Garment(db.model):
     __table_name__ = 'Garment'
-    extend_existing = True
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    user_id = db.Column(db.Integer)
     img_url = db.Column(db.String(350), unique=True)
+    tags = db.relationship('Tag', backref='garment', lazy='dynamic')
 
     def __repr__(self):
         return '<Garment {}>'.format(self.garment_id)
--- app/models/tag.py
+++ app/models/tag.py
@@ -4,7 +4,6 @@ from app import db
 
 class Tag(db.Model):
     __table_name__ = 'Tag'
-    extend_existing = True
     tag_id = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String(100), unique=True)
     garment_id = db.Column(db.Integer, db.ForeignKey('garment.id'))
